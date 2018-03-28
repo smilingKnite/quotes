4ca52df863464e48661ea64b1defec46225c926d
Tyler Mondragon <tydragon4@gmail.com>
2018-03-28 16:41:59 -0700

    updated setting for deployment

--- main/main/settings.py
+++ main/main/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 't6sblagb5j_dyic$z^v5%(y#r$3d7^e_f6ww)hgsarn2yi5v^n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['13.56.159.51', 'localhost']
 
 # Application definition
